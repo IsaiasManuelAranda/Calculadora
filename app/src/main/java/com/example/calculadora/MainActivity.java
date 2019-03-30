49a15a1f919389035e9b46c897c1d50db61c6d28
IsaiasArandaLeal <1717110610@utectulancingo.edu.mx>
2019-03-30 15:50:52 -0600

    Métodos para operaciones en Landscape

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -9,8 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //Variables de EditText
+    //Variables de EditText portraid
     public EditText num1, num2, res;
+
+    //Variables de EditText Landscape
+    public EditText num1_land, num_2_land, res_land;
 
     //Variables de botones
     public Button sum, rest, mult, div, modul;
@@ -27,11 +30,18 @@ public class MainActivity extends AppCompatActivity {
         num2 = (EditText) findViewById(R.id.et_num2);
         res = (EditText) findViewById(R.id.et_res);
 
+        num1_land = (EditText) findViewById(R.id.et_num1_land);
+        num_2_land = (EditText) findViewById(R.id.et_num2_land);
+        res_land = (EditText) findViewById(R.id.et_res_land);
+
+
         sum = (Button) findViewById(R.id.btn_sum);
         rest = (Button) findViewById(R.id.btn_rest);
         mult = (Button) findViewById(R.id.btn_mult);
         div = (Button) findViewById(R.id.btn_div);
         modul = (Button) findViewById(R.id.btn_modul);
+
+
     }
 
     public void sum(View view) {
@@ -43,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
         res.setText(result.toString());
     }
 
+    public void sum_land(View view){
+        numero1 = Float.parseFloat((num1_land.getText().toString()));
+        numero2 = Float.parseFloat((num_2_land.getText().toString()));
+
+        result = numero1 + numero2;
+
+        res_land.setText(result.toString());
+    }
+
     public void rest(View view){
         numero1 =  Float.parseFloat(num1.getText().toString());
         numero2 =  Float.parseFloat(num2.getText().toString());
@@ -50,6 +69,15 @@ public class MainActivity extends AppCompatActivity {
         result = (numero1 - numero2);
 
         res.setText(result.toString());
+    }
+
+    public void rest_land(View view){
+        numero1 = Float.parseFloat((num1_land.getText().toString()));
+        numero2 = Float.parseFloat((num_2_land.getText().toString()));
+
+        result = numero1 - numero2;
+
+        res_land.setText(result.toString());
     }
 
     public void mult(View view){
@@ -61,6 +89,15 @@ public class MainActivity extends AppCompatActivity {
         res.setText(result.toString());
     }
 
+    public void mult_land(View view){
+        numero1 = Float.parseFloat((num1_land.getText().toString()));
+        numero2 = Float.parseFloat((num_2_land.getText().toString()));
+
+        result = numero1 * numero2;
+
+        res_land.setText(result.toString());
+    }
+
     public void div(View view){
         numero1 =  Float.parseFloat(num1.getText().toString());
         numero2 =  Float.parseFloat(num2.getText().toString());
@@ -70,6 +107,15 @@ public class MainActivity extends AppCompatActivity {
         res.setText(result.toString());
     }
 
+    public void div_land(View view){
+        numero1 = Float.parseFloat((num1_land.getText().toString()));
+        numero2 = Float.parseFloat((num_2_land.getText().toString()));
+
+        result = numero1 / numero2;
+
+        res_land.setText(result.toString());
+    }
+
     public void modul(View view){
         numero1 =  Float.parseFloat(num1.getText().toString());
         numero2 =  Float.parseFloat(num2.getText().toString());
@@ -77,5 +123,14 @@ public class MainActivity extends AppCompatActivity {
         result = numero1 % numero2;
 
         res.setText(result.toString());
+    }
+
+    public void modul_land(View view){
+        numero1 = Float.parseFloat((num1_land.getText().toString()));
+        numero2 = Float.parseFloat((num_2_land.getText().toString()));
+
+        result = numero1 % numero2;
+
+        res_land.setText(result.toString());
     }
 }
